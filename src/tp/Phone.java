a0495f66e56d799da1d3dfe5b936da0f3043db3f
Yasser Chenik <yasserchenik218@gmail.com>
2023-12-14 13:04:28 +0100

    Null pointer exception removed

--- src/tp/Phone.java
+++ src/tp/Phone.java
@@ -46,85 +46,85 @@ public class Phone implements Comparable<Phone>{
 	
 	
 
-		    public static void main(String[] args) {
-		        // Création d'objets Phone
-		        Phone tele1 = new SmartPhone("iphone1", 2000, 2023);
-		        Phone tele2 = new Phone("nokiapile", 200, 1999);
-		        Phone tele3 = new Phone("androidphone", 500, 2005);
+	public static void main(String[] args) {
+		// Création d'objets Phone
+		Phone tele1 = new SmartPhone("iphone1", 2000, 2023);
+		Phone tele2 = new Phone("nokiapile", 200, 1999);
+		Phone tele3 = new Phone("androidphone", 500, 2005);
 
-		        // Création d'un objet PhoneListContainer
-		        PhoneListContainer phoneListContainer = new PhoneListContainer(nom, numserie, numserie);
+		// Création d'un objet PhoneListContainer
+		PhoneListContainer phoneListContainer = new PhoneListContainer(nom, numserie, numserie);
 
-		        // Remplissage de la liste de téléphones dans PhoneListContainer
-		        phoneListContainer.populateList(new Phone[]{tele1, tele2, tele3});
+		// Remplissage de la liste de téléphones dans PhoneListContainer
+		phoneListContainer.populateList(new Phone[]{tele1, tele2, tele3});
 
-		        // Affichage de la liste de téléphones
-		        System.out.println("Liste de téléphones :");
-		        phoneListContainer.displayList();
+		// Affichage de la liste de téléphones
+		System.out.println("Liste de téléphones :");
+		phoneListContainer.displayList();
 
-		        // Tri de la liste de téléphones
-		        System.out.println("\nListe de téléphones triée :");
-		        phoneListContainer.sortList();
-		        phoneListContainer.displayList();
+		// Tri de la liste de téléphones
+		System.out.println("\nListe de téléphones triée :");
+		phoneListContainer.sortList();
+		phoneListContainer.displayList();
 
-		        // Insertion d'un nouveau téléphone 
-		        Phone tele4 = new Phone("j1", 300, 2022);
-		        phoneListContainer.insertElement(tele4, 1);
-		        System.out.println("\nListe de téléphones après insertion d'un nouveau téléphone :");
-		        phoneListContainer.displayList();
+		// Insertion d'un nouveau téléphone 
+		Phone tele4 = new Phone("j1", 300, 2022);
+		phoneListContainer.insertElement(tele4, 1);
+		System.out.println("\nListe de téléphones après insertion d'un nouveau téléphone :");
+		phoneListContainer.displayList();
 
-		        // Suppression d'un téléphone
-		        phoneListContainer.removeElement(tele2);
-		        System.out.println("\nListe de téléphones après suppression d'un téléphone :");
-		        phoneListContainer.displayList();
+		// Suppression d'un téléphone
+		phoneListContainer.removeElement(tele2);
+		System.out.println("\nListe de téléphones après suppression d'un téléphone :");
+		phoneListContainer.displayList();
 
-		        Phone nouveauPhone = null;
-				// Recherche d'un téléphone
-		        System.out.println("\n'NouveauPhone' est-il dans la liste ? " + phoneListContainer.searchElement(nouveauPhone));
+		Phone nouveauPhone = null;
+		// Recherche d'un téléphone
+		System.out.println("\n'NouveauPhone' est-il dans la liste ? " + phoneListContainer.searchElement(nouveauPhone));
 
-		        // Copie de la liste de téléphones
-		        List<Phone> listeCopiee = phoneListContainer.copyList();
-		        System.out.println("\nListe de téléphones copiée :");
-		        listeCopiee.forEach(System.out::println);
+		// Copie de la liste de téléphones
+		List<Phone> listeCopiee = phoneListContainer.copyList();
+		System.out.println("\nListe de téléphones copiée :");
+		listeCopiee.forEach(System.out::println);
 
-		        // Mélange de la liste de téléphones
-		        phoneListContainer.shuffleList();
-		        System.out.println("\nListe de téléphones mélangée :");
-		        phoneListContainer.displayList();
+		// Mélange de la liste de téléphones
+		phoneListContainer.shuffleList();
+		System.out.println("\nListe de téléphones mélangée :");
+		phoneListContainer.displayList();
 
-		        // Inversion de la liste de téléphones
-		        phoneListContainer.reverseList();
-		        System.out.println("\nListe de téléphones inversée :");
-		        phoneListContainer.displayList();
+		// Inversion de la liste de téléphones
+		phoneListContainer.reverseList();
+		System.out.println("\nListe de téléphones inversée :");
+		phoneListContainer.displayList();
 
-		        // Extraction d'une sous-liste
-		        List<Phone> sousListe = phoneListContainer.subList(1, 3);
-		        System.out.println("\nSous-liste (de l'index 1 à 3) :");
-		        sousListe.forEach(System.out::println);
+		// Extraction d'une sous-liste
+		List<Phone> sousListe = phoneListContainer.subList(1, 3);
+		System.out.println("\nSous-liste (de l'index 1 à 3) :");
+		sousListe.forEach(System.out::println);
 
-		        // Comparaison de listes
-		        List<Phone> autreListe = Arrays.asList(tele1, tele2, tele3);
-		        System.out.println("\nLes deux listes sont-elles égales ? " + phoneListContainer.compareLists(autreListe));
+		// Comparaison de listes
+		List<Phone> autreListe = Arrays.asList(tele1, tele2, tele3);
+		System.out.println("\nLes deux listes sont-elles égales ? " + phoneListContainer.compareLists(autreListe));
 
-		        // Échange d'éléments aux indices 0 et 2
-		        phoneListContainer.swapElements(0, 2);
-		        System.out.println("\nListe de téléphones après échange d'éléments aux indices 0 et 2 :");
-		        phoneListContainer.displayList();
+		// Échange d'éléments aux indices 0 et 2
+		phoneListContainer.swapElements(0, 2);
+		System.out.println("\nListe de téléphones après échange d'éléments aux indices 0 et 2 :");
+		phoneListContainer.displayList();
 
-		        // Vidange de la liste de téléphones
-		        phoneListContainer.clearList();
-		   
-		        System.out.println("\nLa liste de téléphones est-elle vide ? " + phoneListContainer.isListEmpty());
-		    
-		        Phone tele88 = new SmartPhone("iphone1", 2000, 2023);
-				Phone tele5 = new Phone("nokiapile", 200,1999);
-				
-				
-				System.out.println(tele88);
-				System.out.println(tele5);
-		    
-		    }
+		// Vidange de la liste de téléphones
+		phoneListContainer.clearList();
+	
+		System.out.println("\nLa liste de téléphones est-elle vide ? " + phoneListContainer.isListEmpty());
+	
+		Phone tele88 = new SmartPhone("iphone1", 2000, 2023);
+		Phone tele5 = new Phone("nokiapile", 200,1999);
 		
+		
+		System.out.println(tele88);
+		System.out.println(tele5);
+	
+	}
+
 
 		
 	}
--- src/tp/PhoneListContainer.java
+++ src/tp/PhoneListContainer.java
@@ -6,18 +6,17 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-public class PhoneListContainer extends Phone{
-	
-	
+public class PhoneListContainer extends Phone {
+
 	private List<Phone> phoneList;
 
-	//constructeur
-	
-	 public PhoneListContainer(String nom, int prix, int numserie) {
+	// constructeur
+
+	public PhoneListContainer(String nom, int prix, int numserie) {
 		super(nom, prix, numserie);
+		phoneList = new ArrayList<Phone>();
 	}
 
-	
 	public List<Phone> getPhoneList() {
 		return phoneList;
 	}
@@ -25,99 +24,99 @@ public class PhoneListContainer extends Phone{
 	public void setPhoneList(List<Phone> phoneList) {
 		this.phoneList = phoneList;
 	}
-	
-	
 
-	   // a. Fonction pour alimenter la liste
-	 
-	    public void populateList(Phone[] phones) {
-	        phoneList.addAll(Arrays.asList(phones));
-	    }
+	// a. Fonction pour alimenter la liste
 
-	    // b. Fonction pour afficher la liste (ligne par ligne)
-	    
-	    
-	    public void displayList() {
-	        for (Phone phone : phoneList) {
-	            System.out.println(phone);
-	        }
-	    }
-	    // c. Fonction pour parcourir tous les éléments de la liste à l'aide d'un Iterateur
-	    
-	    public void iterateList() {
-	        Iterator<Phone> iterator = phoneList.iterator();
-	        while (iterator.hasNext()) {
-	            
-	        }
-	    }
-	    // d. Fonction pour insérer un élément dans la liste
-	    
-	    public void insertElement(Phone phone, int index) {
-	        phoneList.add(index, phone);
-	    }
+	public void populateList(Phone[] phones) {
+		phoneList.addAll(Arrays.asList(phones));
+	}
 
-	    // e. Codez une fonction Java qui permet de récupérer un élément 
-	    
-	    public Phone getElement(int index) {
-	        return phoneList.get(index);
-	    }
-	    // f. Codez une fonction Java qui permet de supprimer un élément.
-	   
-	    public void removeElement(Phone phone) {
-	        phoneList.remove(phone);
-	    }
-	    // g. Codez une fonction Java qui permet de rechercher un élément.
-	   
-	    public boolean searchElement(Phone phone) {
-	        return phoneList.contains(phone);
-	    }
-	    
-	    // h. Codez une fonction Java qui permet de trier la liste
-	    public void sortList() {
-	    	Collections.sort(phoneList);
-	    }
+	// b. Fonction pour afficher la liste (ligne par ligne)
 
-	
-		// i. Codez une fonction Java qui permet de copier la liste dans un nouveau tableau.
-		  
-		public List<Phone> copyList() {
-		        return new ArrayList<>(phoneList);
-		    }
-		  // j. Codez une fonction Java qui permet de mélanger les éléments de la liste.
-	    
-		public void shuffleList() {
-	        Collections.shuffle(phoneList);
-	    }
+	public void displayList() {
+		for (Phone phone : phoneList) {
+			System.out.println(phone);
+		}
+	}
+	// c. Fonction pour parcourir tous les éléments de la liste à l'aide d'un
+	// Iterateur
 
-	    // k. Codez une fonction Java qui permet d’inverser les éléments de la liste.
-	   
-	    public void reverseList() {
-	        Collections.reverse(phoneList);
-	    }
+	public void iterateList() {
+		Iterator<Phone> iterator = phoneList.iterator();
+		while (iterator.hasNext()) {
 
-	    // l. Codez une fonction Java qui permet d’extraire une partie de la liste.
-	   
-	    public List<Phone> subList(int fromIndex, int toIndex) {
-	        return phoneList.subList(fromIndex, toIndex);
-	    }
+		}
+	}
+	// d. Fonction pour insérer un élément dans la liste
 
-	    // m. Codez une fonction Java qui permet de comparer deux listes.
-	    
-	    public boolean compareLists(List<Phone> otherList) {
-	        return phoneList.equals(otherList);
-	    }
-	 // n. Codez une fonction Java d'échange de deux éléments dans une liste.
-	    public void swapElements(int iphone7, int iphone8) {
-	        Collections.swap(phoneList, iphone7, iphone8);
-	    }
-	 // o. Codez une fonction Java qui permet de vider la liste.
-	    public void clearList() {
-	        phoneList.clear();
-	    }
-	 // p. Codez une fonction Java qui permet de tester que la liste est vide ou non.
-	    
-	    public boolean isListEmpty() {
-	        return phoneList.isEmpty();
-	    }
-	    
+	public void insertElement(Phone phone, int index) {
+		phoneList.add(index, phone);
+	}
+
+	// e. Codez une fonction Java qui permet de récupérer un élément
+
+	public Phone getElement(int index) {
+		return phoneList.get(index);
+	}
+	// f. Codez une fonction Java qui permet de supprimer un élément.
+
+	public void removeElement(Phone phone) {
+		phoneList.remove(phone);
+	}
+	// g. Codez une fonction Java qui permet de rechercher un élément.
+
+	public boolean searchElement(Phone phone) {
+		return phoneList.contains(phone);
+	}
+
+	// h. Codez une fonction Java qui permet de trier la liste
+	public void sortList() {
+		Collections.sort(phoneList);
+	}
+
+	// i. Codez une fonction Java qui permet de copier la liste dans un nouveau
+	// tableau.
+
+	public List<Phone> copyList() {
+		return new ArrayList<>(phoneList);
+	}
+	// j. Codez une fonction Java qui permet de mélanger les éléments de la liste.
+
+	public void shuffleList() {
+		Collections.shuffle(phoneList);
+	}
+
+	// k. Codez une fonction Java qui permet d’inverser les éléments de la liste.
+
+	public void reverseList() {
+		Collections.reverse(phoneList);
+	}
+
+	// l. Codez une fonction Java qui permet d’extraire une partie de la liste.
+
+	public List<Phone> subList(int fromIndex, int toIndex) {
+		return phoneList.subList(fromIndex, toIndex);
+	}
+
+	// m. Codez une fonction Java qui permet de comparer deux listes.
+
+	public boolean compareLists(List<Phone> otherList) {
+		return phoneList.equals(otherList);
+	}
+
+	// n. Codez une fonction Java d'échange de deux éléments dans une liste.
+	public void swapElements(int iphone7, int iphone8) {
+		Collections.swap(phoneList, iphone7, iphone8);
+	}
+
+	// o. Codez une fonction Java qui permet de vider la liste.
+	public void clearList() {
+		phoneList.clear();
+	}
+	// p. Codez une fonction Java qui permet de tester que la liste est vide ou non.
+
+	public boolean isListEmpty() {
+		return phoneList.isEmpty();
+	}
+
 }
