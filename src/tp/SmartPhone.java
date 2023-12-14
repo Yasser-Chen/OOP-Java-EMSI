5c4bd133a7ba76a483b68fadc51478c81a15af5b
Yasser Chenik <yasserchenik218@gmail.com>
2023-12-14 13:09:35 +0100

    removed auto-generated comments

--- src/tp/SmartPhone.java
+++ src/tp/SmartPhone.java
@@ -1,13 +1,9 @@
 package tp;
 
-public class SmartPhone extends Phone  {
+public class SmartPhone extends Phone {
 
 	public SmartPhone(String nom, int prix, int numserie) {
 		super(nom, prix, numserie);
-		// TODO Auto-generated constructor stub
 	}
 
-
-	
-	
 }
